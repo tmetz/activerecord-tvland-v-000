19bdfbbcc93a99209b1115a5b0a06d040a81b6c9
Tammy Metz <tammy@tammymetz.com>
2018-10-06 16:25:09 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 5) do
+
+  create_table "actors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+  end
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.integer "show_id"
+    t.integer "actor_id"
+  end
+
+  create_table "networks", force: :cascade do |t|
+    t.string "call_letters"
+    t.integer "channel"
+  end
+
+  create_table "shows", force: :cascade do |t|
+    t.string "name"
+    t.string "genre"
+    t.integer "network_id"
+  end
 
 end
